a3de49b3f19e16571375e589be84df9f99a909c2
Mansur Ul Hasan <mansoor.hasan@10pearls.com>
2020-12-09 03:34:27 +0500

    Adding verification api

--- web/ScenarioVerify.py
+++ web/ScenarioVerify.py
@@ -5,8 +5,8 @@ from flask_session import Session
 import requests
 import os
 from ContainerCommit import containercommit
-import fnmatch
-import subprocess
+import io
+from contextlib import redirect_stdout
 
 # create the application object
 app = Flask(__name__)
@@ -18,18 +18,23 @@ app.config.from_pyfile(os.path.join("..", "conf/app.conf"), silent=False)
 containerverify = Blueprint('containerverify', __name__)
 
 @containerverify.route('/verify', methods=['GET', 'POST', 'UPDATE'])
-def index():
+def apache2():        
     if session.get('loggedin') is not None:
         client = docker.from_env()
         ContainerName = session['user']
         g.csID = session['user']
         command = client.containers.get(g.csID)
-        apache2 = command.exec_run('dpkg -l')
-        # ListWeb = 'apache2'
-        # filtered = fnmatch.filter(apache2, 'apache?') 
-        output = subprocess.check_output(['dpkg', '-l'])
-        for output in apache2.split('\n'):
-            print(output)        
+        firstString = []
+        apache2 = command.exec_run("dpkg -l apache2")
+        list_string = str(apache2)
+        final = list_string.find('code=0')
+        if final > 0:
+            print('Package is installed')
+        else:
+            print('Not Installed')
+        return 'end'       
     else:
         return redirect('/login')
+
+
 
